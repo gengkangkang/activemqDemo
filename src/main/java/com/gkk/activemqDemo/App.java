67b7603337b33f766a0178b713e9494d853bcaa4
gengkangkang <kangkang.geng@foxmail.com>
2015-12-06 00:10:02 +0800

    mq例子

---
+++ src/main/java/com/gkk/activemqDemo/App.java
@@ -0,0 +1,13 @@
+package com.gkk.activemqDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
